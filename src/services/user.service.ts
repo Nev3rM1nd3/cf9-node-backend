fef29cc6d657d55088b547404be6a564574baed8
Nev3rM1nd3 <panorg264@gmail.com>
2026-04-07 21:03:43 +0300

    add: app.use

---
+++ src/services/user.service.ts
@@ -0,0 +1,11 @@
+import User, {IUser} from '../models/user.model';
+import Role, {IRole} from '../models/role.model';
+
+import {Types} from 'mongoose';
+
+export const createUser = async(payload:IUser) => {
+  console.log("Service Payload", payload);
+  const user = new User(payload);
+  console.log("New user: ", user)
+  return user;
+}
